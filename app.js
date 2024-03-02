963a8bfdb8328f867d6c2aba9aa33dd417232988
Malik Aaqil <malikaaqil2000@gmail.com>
2024-03-02 16:07:34 +0500

    updated app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let timer = document.getElementById("timer");
+
+let startBtn = document.getElementById("startBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+let resetBtn = document.getElementById("resetBtn");
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+let flag = true;
